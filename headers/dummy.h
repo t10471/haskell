0fb2034d549aff0374d40fca35ec637b9a0f33c0
t10471 <t104711202@gmail.com>
2014-12-11 19:33:10 +0900

    added headers

---
+++ headers/dummy.h
@@ -0,0 +1,7 @@
+#define VERSION_accelerate "0.19.0"
+
+#define MIN_VERSION_accelerate(major1,major2,minor) (\
+  (major1) <  0 || \
+  (major1) == 0 && (major2) <  19 || \
+  (major1) == 0 && (major2) == 19 && (minor) <= 0)
+
